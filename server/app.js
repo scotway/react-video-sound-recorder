f9f1943047f524d9d6bd98a0610d42d90ba827f3
Scott Conway <scott.b.conway@gmail.com>
2018-07-16 14:26:41 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -10,34 +10,9 @@ app.set('view engine', 'hbs');
 // parse application/json
 app.use(bodyParser.json());
 
-/*
-app.put('/v2/rtn/users/:userId(\\d+)', async (req, res, next) => {
-  try {
-    req.appResponseData = await UserController.update(req);
-    next();
-  } catch (error) {
-    req.appResponseError = error;
-    next();
-  }
-});
-*/
 
 app.get('/', (req, res, next) => {
   res.render('index',{});
 });
-
-/*
-app.use((req, res) => {
-  if (req.appResponseData) {
-    res.status(200).json(req.appResponseData);
-  } else if (req.appResponseError) {
-    res.status(400).json({
-      error: req.appResponseError,
-    });
-  } else {
-    res.status(404).send('Not Found');
-  }
-});
-*/
 
 module.exports = app;
